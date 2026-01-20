0eeb702f103292f14923e18ec441d8e9456fd301
SSkk999 <cvantymgeymshhhhh@gmail.com>
2026-01-20 18:48:25 +0200

    favorit_list

--- favorites/urls.py
+++ favorites/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     # path('', views.index, name='favorites_index'),
     path('add/<int:car_id>/<str:return_url>/', views.add_car, name='add_fav_car'),
     path('remove/<int:car_id>/<str:return_url>/', views.remove_car, name='remove_fav_car'),
+    path('list/', views.car_favorites_cat, name='favorites_list'),
 ]
--- favorites/views.py
+++ favorites/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import redirect, render
-from favorites.favorites import add_car_to_favorites, remove_car_from_favorites
+from favorites.favorites import add_car_to_favorites, remove_car_from_favorites,get_favorite_car,get_count_of_favorite_car
+from cars_menu.models import Car
 
 def add_car(request, car_id, return_url):
     add_car_to_favorites(request, car_id)
@@ -7,4 +8,15 @@ def add_car(request, car_id, return_url):
 
 def remove_car(request, car_id, return_url):
     remove_car_from_favorites(request, car_id)
-    return redirect(return_url)
+    return redirect(request.META.get('HTTP_REFERER', '/'))
+
+def car_favorites_cat(request):
+    carsf = get_favorite_car(request)
+    cars = Car.objects.filter(id__in=carsf)
+    return render(request, 'favorites_list.html', {
+        'cars': cars, 
+        "fav_count": get_count_of_favorite_car(request),
+        "fav_ids": get_favorite_car(request),
+     }
+     )
+
